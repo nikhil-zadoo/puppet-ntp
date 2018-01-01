a7fad5a8d3e63d77bc25904615af36d46c2d7d9a
root <root@puppetmaster.mylabserver.com>
2018-01-01 23:52:06 +0100

    making first commit

---
+++ manifests/service.pp
@@ -0,0 +1,14 @@
+class ntp::service (
+  String $service_name        = $ntp::service_name,
+  String $service_ensure      = $ntp::service_ensure,
+  Boolean $service_enable     = $ntp::service_enable,
+  Boolean $service_hasstatus  = $ntp::service_hasstatus,
+  Boolean $service_hasrestart = $ntp::service_hasrestart,
+){
+  service {$service_name:
+    ensure     => $service_ensure,
+    enable     => $service_enable,
+    haststatus => $service_hasstatus,
+    hasrestart => $service_hasrestart,
+  }
+}
